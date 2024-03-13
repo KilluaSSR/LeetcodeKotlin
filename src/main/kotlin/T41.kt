2618cc5585c2913353d08ea1d2bcf0949b20929d
KilluaSSR <xiaotian2001@outlook.com>
2024-03-13 19:11:08 +0800

    Mar13

--- src/main/kotlin/T41.kt
+++ src/main/kotlin/T41.kt
@@ -2,22 +2,22 @@ import java.math.BigInteger
 
 fun firstMissingPositive(nums: IntArray): Int {
     val n = nums.size
-    var i:Int = 0
-    while (i<n){
-        if(nums[i] in 1..n && nums[nums[i]-1]!=nums[i]){
-            val tmp = nums[nums[i]-1]
-            nums[nums[i]-1] = nums[i]
-            nums[i] = tmp
+    var i = 0
+    while (i < n) {
+        while (nums[i] in 1..n && nums[nums[i] - 1] != nums[i]) {
+            val temp = nums[nums[i] - 1]
+            nums[nums[i] - 1] = nums[i]
+            nums[i] = temp
         }
         i++
     }
-    for (k in nums.indices){
-        if(nums[k]!=k+1){
-            return k+1
+    for (i in nums.indices) {
+        if (nums[i] != i + 1) {
+            return i + 1
         }
     }
-    return n+1
+    return n + 1
 }
 fun main() {
-    println(firstMissingPositive(intArrayOf(-2,-1,3,7,1,4)))
+    println(firstMissingPositive(intArrayOf(3,4,-1,1)))
 }
